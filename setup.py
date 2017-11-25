7ea34b7ade41170cbbac3a80b38a5254951b6c08
Sepehr Hamzehlouy <s.hamzelooy@gmail.com>
2017-11-26 00:39:52 +0330

    add darwin os

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
+from sys import platform
+
 from setuptools import setup, Extension
 
 libraries = ['freetype', 'harfbuzz']
-include_dirs = ['/usr/include/harfbuzz/', '/usr/include/freetype2/']
+
+if platform == "linux" or platform == "linux2":
+    include_dirs = ['/usr/include/harfbuzz/', '/usr/include/freetype2/']
+elif platform == "darwin":
+    include_dirs = ['/usr/local/include/harfbuzz', '/usr/local/include/freetype2']
 
 setup(
     name='pyharfbuzz',
-    version='0.0.1',
-    description='A collection of framework independent HTTP protocol utils.',
+    version='0.0.2',
+    description='Python binding for harfbuzz an OpenType text shaping.',
     author='Sepehr Hamzehlouy',
     author_email='s.hamzelooy@gmail.com',
     license='MIT',
