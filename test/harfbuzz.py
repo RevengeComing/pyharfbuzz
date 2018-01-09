a5ad4b1628cf6d92c85d4d22240df80a13d29b3c
Sepehr Hamzehlouy <s.hamzelooy@gmail.com>
2018-01-09 11:43:34 +0330

    Test case name change

--- test/harfbuzz.py
+++ test/harfbuzz.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 
 from pyharfbuzz import *
 
 
-class TestGet(unittest.TestCase):
+class TestHarfbuzz(unittest.TestCase):
     font_name = 'Vazir.ttf'
     unshaped = "یک نوشته ی تستی"
     shaped = "ﯽﺘﺴﺗ ی ﻪﺘﺷﻮﻧ ﮏﯾ"
@@ -19,7 +21,12 @@ class TestGet(unittest.TestCase):
         out_put = shape(self.font_name, self.unshaped)
         shaped = ''.join([c['char'] for c in out_put])
         self.assertEqual(self.shaped, shaped)
-        
+
+    def test_buffer(self):
+        hb_buffer = HBBufferT()
+        hb_buffer.hb_buffer_add_utf8(self.unshaped, -1, 0, -1)
+        hb_buffer2 = hb_buffer_reference(hb_buffer)
+
 
 if __name__ == '__main__':
     unittest.main()
